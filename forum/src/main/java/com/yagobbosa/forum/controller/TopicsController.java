45ce1ae09339105063054ae1af8f6c0fca1ff594
yagobbosa <yagobbosa@yahoo.com>
2022-04-25 09:09:33 -0300

    add topic form to form

--- forum/src/main/java/com/yagobbosa/forum/controller/TopicsController.java
+++ forum/src/main/java/com/yagobbosa/forum/controller/TopicsController.java
@@ -3,24 +3,34 @@ package com.yagobbosa.forum.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yagobbosa.forum.controller.dto.TopicDto;
+import com.yagobbosa.forum.controller.form.TopicForm;
 import com.yagobbosa.forum.model.Topic;
 import com.yagobbosa.forum.repository.TopicRepository;
 
 @RestController
+@RequestMapping("/topics")
 public class TopicsController {
 
 	@Autowired
 	private TopicRepository topicRepository;
-
-	@RequestMapping("/topics")
+	
+	@GetMapping
 	public List<TopicDto> list() {
 		List<Topic> topic = topicRepository.findAll();
 
 		return TopicDto.toConvert(topic);
+	}
+	
+	@PostMapping
+	public void register(@RequestBody TopicForm form) {
+		
 	}
 
 }
---
+++ forum/src/main/java/com/yagobbosa/forum/controller/form/TopicForm.java
@@ -0,0 +1,33 @@
+package com.yagobbosa.forum.controller.form;
+
+public class TopicForm {
+
+	private String title;
+	private String message;
+	private String courseName;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+}
