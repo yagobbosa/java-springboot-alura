cd4d4b67c85738ef655fe32180cd693e4b3b691e
yagobbosa <yagobbosa@yahoo.com>
2022-04-24 22:52:07 -0300

    add persistence annotations

--- forum/src/main/java/com/yagobbosa/forum/model/Answer.java
+++ forum/src/main/java/com/yagobbosa/forum/model/Answer.java
@@ -2,12 +2,25 @@ package com.yagobbosa.forum.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Answer {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String message;
+	
+	@ManyToOne
 	private Topic topic;
 	private LocalDateTime creationDate = LocalDateTime.now();
+	
+	@ManyToOne
 	private User author;
 	private Boolean solved = false;
 
--- forum/src/main/java/com/yagobbosa/forum/model/Topic.java
+++ forum/src/main/java/com/yagobbosa/forum/model/Topic.java
@@ -4,14 +4,35 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Topic {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private String message;
 	private LocalDateTime creationDate = LocalDateTime.now();
+
+	@Enumerated(EnumType.STRING)
+	private TopicStatus status = TopicStatus.UNSOLVED;
+
+	@ManyToOne
 	private User author;
+
+	@ManyToOne
 	private Course course;
+
+	@OneToMany(mappedBy = "topic")
 	private List<Answer> answer = new ArrayList<>();
 
 	public Topic(String title, String message, Course course) {
@@ -81,6 +102,14 @@ public class Topic {
 
 	public void setCreationDate(LocalDateTime creationDate) {
 		this.creationDate = creationDate;
+	}
+
+	public TopicStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(TopicStatus status) {
+		this.status = status;
 	}
 
 	public User getAuthor() {
