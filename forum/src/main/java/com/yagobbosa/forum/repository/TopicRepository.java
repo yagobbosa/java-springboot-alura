40bf3b7c7a1a371a3e3eccff3e4495510f67eb1d
yagobbosa <yagobbosa@yahoo.com>
2022-04-25 09:34:39 -0300

    topic repository implementation

--- forum/src/main/java/com/yagobbosa/forum/repository/TopicRepository.java
+++ forum/src/main/java/com/yagobbosa/forum/repository/TopicRepository.java
@@ -1,9 +1,13 @@
 package com.yagobbosa.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yagobbosa.forum.model.Topic;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+
+	List<Topic> findByCourseName(String courseName);
 
 }
