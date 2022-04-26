e527a0cae3220bb680228ec4793144acb8b8f147
yagobbosa <yagobbosa@yahoo.com>
2022-04-26 20:35:54 -0300

    implementing jwt token generation

---
+++ forum/src/main/java/com/yagobbosa/forum/config/security/TokenService.java
@@ -0,0 +1,32 @@
+package com.yagobbosa.forum.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import com.yagobbosa.forum.model.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+
+	@Value("${forum.jwt.expiration}")
+	private String expiration;
+
+	@Value("${forum.jwt.secret}")
+	private String secret;
+
+	public String generateToken(Authentication authentication) {
+		User logged = (User) authentication.getPrincipal();
+		Date date = new Date();
+		Date expirationDate = new Date(date.getTime() + Long.parseLong(expiration));
+
+		return Jwts.builder().setIssuer("forum API").setSubject(logged.getId().toString()).setIssuedAt(date)
+				.setExpiration(expirationDate).signWith(SignatureAlgorithm.HS256, secret).compact();
+	}
+
+}
---
+++ forum/src/main/java/com/yagobbosa/forum/controller/AuthenticationController.java
@@ -0,0 +1,45 @@
+package com.yagobbosa.forum.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yagobbosa.forum.config.security.TokenService;
+import com.yagobbosa.forum.controller.form.LoginForm;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private TokenService tokenService;
+
+	@PostMapping
+	public ResponseEntity<?> authentication(@RequestBody @Valid LoginForm form) {
+		UsernamePasswordAuthenticationToken loginData = form.toConvert();
+
+		try {
+			Authentication authentication = authenticationManager.authenticate(loginData);
+
+			String token = tokenService.generateToken(authentication);
+
+			return ResponseEntity.ok().build();
+		} catch (AuthenticationException e) {
+			return ResponseEntity.badRequest().build();
+		}
+
+	}
+
+}
