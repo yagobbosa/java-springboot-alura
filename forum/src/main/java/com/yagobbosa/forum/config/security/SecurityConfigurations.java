1e078cc0ffbfdfe00acef540e81c697a41110a44
yagobbosa <yagobbosa@yahoo.com>
2022-04-28 23:18:12 -0300

    add dev and prod profiles

--- forum/src/main/java/com/yagobbosa/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/com/yagobbosa/forum/config/security/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package com.yagobbosa.forum.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -21,6 +22,7 @@ import com.yagobbosa.forum.repository.UserRepository;
 
 @EnableWebSecurity
 @Configuration
+@Profile("prod")
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	@Autowired
